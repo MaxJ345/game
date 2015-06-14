6f1c80f4281c5b091381c9478166db913dc6f973
MaxJ345 <maxj345@yahoo.com>
2015-06-13 21:08:09 -0400

    Cleans up roomGen and roomDelete. Adds test of the two together.

--- roomDelete.cpp
+++ roomDelete.cpp
@@ -14,6 +14,8 @@ struct room {
 } ;
 
 int roomDelete(room *, char);
+void seek(room *, char);
+void sever_connections(room * room_ptr, char coord);
 
 int main(void)
 {
@@ -33,50 +35,24 @@ int roomDelete(room * room_ptr, char coord)
 	char coordinate_opposite[NUM_DOORS] = {'S', 'N', 'W', 'E'};
 	char coord_opp = '\0';
 	
-	// call function on any remaining rooms
+	// base case
 	if(coord == '\0')	// this is the beginning/initial room
 	{
-		for(int i = 0; i < NUM_DOORS; i++)
-		{
-			switch (coordinate[i])
-			{
-				case 'N':
-				{
-					if(room_ptr->north != NULL)
-						roomDelete(room_ptr->north, 'N');
-					break;
-				}
-				case 'S':
-				{
-					if(room_ptr->south != NULL)
-						roomDelete(room_ptr->south, 'S');
-					break;
-				}
-				case 'E':
-				{
-					if(room_ptr->east != NULL)
-						roomDelete(room_ptr->east, 'E');
-					break;
-				}
-				case 'W':
-				{
-					if(room_ptr->west != NULL)
-						roomDelete(room_ptr->west, 'W');
-					break;
-				}
-				default:
-					cout << "There was an error deallocating for the room at location: " << room_ptr << endl;
-			}
-		}
+		// call the function on any remaining rooms
+		seek(room_ptr, '\0');
 		
 		// delete current room
 		if(room_ptr->north == NULL && room_ptr->south == NULL && room_ptr->east == NULL && room_ptr->west == NULL)
+		{
+			cout << "Deleting room at location: " << room_ptr << endl;
 			delete room_ptr;
+		}
 		else
 			return 2;		// outward rooms have not been deleted yet
 	}		
 	
-	else		// recursion
+	// recursion
+	else
 	{
 		// this sets the value for the door that won't be handed to the delete function
 		for(int j = 0; j < NUM_DOORS; j++)
@@ -92,79 +68,80 @@ int roomDelete(room * room_ptr, char coord)
 		}
 		
 		// call function on any remaining rooms
-		for(int k = 0; k < NUM_DOORS; k++)
-		{
-			if(coordinate[k] != coord_opp)		// this is to avoid backtracking (which would probably cause infinite recursion)
-			{
-				switch (coordinate[k])
-				{
-					case 'N':
-					{
-						if(room_ptr->north != NULL)
-							roomDelete(room_ptr->north, 'N');
-						break;
-					}
-					case 'S':
-					{
-						if(room_ptr->south != NULL)
-							roomDelete(room_ptr->south, 'S');
-						break;
-					}
-					case 'E':
-					{
-						if(room_ptr->east != NULL)
-							roomDelete(room_ptr->east, 'E');
-						break;
-					}
-					case 'W':
-					{
-						if(room_ptr->west != NULL)
-							roomDelete(room_ptr->west, 'W');
-						break;
-					}
-					default:
-						cout << "There was an error deallocating for the room at location: " << room_ptr << endl;
-				}
-			}
-		}
+		seek(room_ptr, coord_opp);
 		
 		// delete connection (ptr's) between current room and previous
-		switch(coord)
-		{
-			case 'N':
-			{
-				room_ptr->south->north = NULL;
-				room_ptr->south = NULL;
-				break;
-			}
-			case 'S':
-			{
-				room_ptr->north->south = NULL;
-				room_ptr->north = NULL;
-				break;
-			}
-			case 'E':
-			{
-				room_ptr->west->east = NULL;
-				room_ptr->west = NULL;
-				break;
-			}
-			case 'W':
-			{
-				room_ptr->east->west = NULL;
-				room_ptr->east = NULL;
-				break;
-			}
-			default:
-				cout << "There was a problem with severing the connection for the room at location: " << room_ptr << endl;
-		}
+		sever_connections(room_ptr, coord);
 		
 		// delete current room
 		if(room_ptr->north == NULL && room_ptr->south == NULL && room_ptr->east == NULL && room_ptr->west == NULL)
+		{
+			cout << "Deleting room at location: " << room_ptr << endl;
 			delete room_ptr;
+		}
 		else
 			return 3;		// outward rooms have not been deleted yet
 	}
 	
 	return 0;	// successful in deallocating the entire complex
+}
+
+// function for seeking which rooms to further delete
+void seek(room * room_ptr, char coord_opp)
+{
+	char coordinate[NUM_DOORS] = {'N', 'S', 'E', 'W'};
+	
+	for(int i = 0; i < NUM_DOORS; i++)
+	{
+		if(coordinate[i] != coord_opp)		// this is to avoid backtracking (which would cause infinite recursion)
+		{
+			switch (coordinate[i])
+			{
+				case 'N':
+					if(room_ptr->north != NULL)
+						roomDelete(room_ptr->north, 'N');
+					break;
+				case 'S':
+					if(room_ptr->south != NULL)
+						roomDelete(room_ptr->south, 'S');
+					break;
+				case 'E':
+					if(room_ptr->east != NULL)
+						roomDelete(room_ptr->east, 'E');
+					break;
+				case 'W':
+					if(room_ptr->west != NULL)
+						roomDelete(room_ptr->west, 'W');
+					break;
+				default:
+					cout << "There was an error deallocating for the room at location: " << room_ptr << endl;
+			}
+		}
+	}
+}
+
+// function that severs the connections between the current room and the previous room
+void sever_connections(room * room_ptr, char coord)
+{
+	switch(coord)
+	{
+		case 'N':
+			room_ptr->south->north = NULL;
+			room_ptr->south = NULL;
+			break;
+		case 'S':
+			room_ptr->north->south = NULL;
+			room_ptr->north = NULL;
+			break;
+		case 'E':
+			room_ptr->west->east = NULL;
+			room_ptr->west = NULL;
+			break;
+		case 'W':
+			room_ptr->east->west = NULL;
+			room_ptr->east = NULL;
+			break;
+		default:
+			cout << "There was a problem with severing the connection for the room at location: " << room_ptr << endl;
+	}
 }
--- test/test_new_roomGen_new_roomDelete.cpp
+++ test/test_new_roomGen_new_roomDelete.cpp
@@ -13,45 +13,22 @@ struct room {
 	struct room * west;
 } ;
 
-int roomDelete(room *, char);
-
-int main(void)
-{
-	return 0;
-}
-
-/*
-roomDelete:
-This function deletes a complex of interlinked 'rooms'.
-The function takes an initial pointer (to a room) and coordinate (should be NULL for initial value) as input.
-The function returns a value based on the success/failure of the function.
-*/
-
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
-
-#define NUM_DOORS 4
-
-struct room {
-	int number;				// simulating content
-	struct room * north;
-	struct room * south;
-	struct room * east;
-	struct room * west;
-} ;
-
 room * roomGen(room *, int, char);
 room * gen_init_room(int);
 void gen_room(room *, int, char);
 void set_direction(room *, char, room *);
 
+int roomDelete(room *, char);
+void seek(room *, char);
+void sever_connections(room * room_ptr, char coord);
+
 int main(void)
 {
 	srand(time(NULL));
 	room * starting_room_ptr = roomGen(NULL, 1, 'E');
+
+	int test = roomDelete(starting_room_ptr, '\0');
+	cout << test << endl;
 }
 
 // A function will have to be written so that we can figure out what the "outermost" rooms are (leaves), then we start deleting them one at a time with 'delete' (moving up the branch) until we get to the initial room (root); this will all be needed for deallocation.
@@ -145,22 +122,147 @@ void set_direction(room * room_ptr, char coord, room * temp_room_ptr)
 	switch (coord)
 	{
 		case 'N':
+		{
 			room_ptr->north = temp_room_ptr;
 			temp_room_ptr->south = room_ptr;
 			break;
+		}
 		case 'S':
+		{
 			room_ptr->south = temp_room_ptr;
 			temp_room_ptr->north = room_ptr;
 			break;
+		}
 		case 'E':
+		{
 			room_ptr->east = temp_room_ptr;
 			temp_room_ptr->west = room_ptr;
 			break;
+		}
 		case 'W':
+		{
 			room_ptr->west = temp_room_ptr;
 			temp_room_ptr->east = room_ptr;
 			break;
+		}
 		default:
 			cout << "There was an error setting pointers for the room at location: " << temp_room_ptr << endl;
+	}
+}
+
+int roomDelete(room * room_ptr, char coord)
+{
+	char coordinate[NUM_DOORS] = {'N', 'S', 'E', 'W'};
+	char coordinate_opposite[NUM_DOORS] = {'S', 'N', 'W', 'E'};
+	char coord_opp = '\0';
+
+	// base case
+	if(coord == '\0')	// this is the beginning/initial room
+	{
+		// call the function on any remaining rooms
+		seek(room_ptr, '\0');
+
+		// delete current room
+		if(room_ptr->north == NULL && room_ptr->south == NULL && room_ptr->east == NULL && room_ptr->west == NULL)
+		{
+			cout << "Deleting room at location: " << room_ptr << endl;
+			delete room_ptr;
+		}
+		else
+			return 2;		// outward rooms have not been deleted yet
+	}
+
+	// recursion
+	else
+	{
+		// this sets the value for the door that won't be handed to the delete function
+		for(int j = 0; j < NUM_DOORS; j++)
+		{
+			if(coord == coordinate[j])
+				coord_opp = coordinate_opposite[j];
+		}
+
+		if(coord_opp == '\0')
+		{
+			cout << "An error occurred while setting the value of the opposite coordinate.\n";
+			return 1;
+		}
+
+		// call function on any remaining rooms
+		seek(room_ptr, coord_opp);
+
+		// delete connection (ptr's) between current room and previous
+		sever_connections(room_ptr, coord);
+
+		// delete current room
+		if(room_ptr->north == NULL && room_ptr->south == NULL && room_ptr->east == NULL && room_ptr->west == NULL)
+		{
+			cout << "Deleting room at location: " << room_ptr << endl;
+			delete room_ptr;
+		}
+		else
+			return 3;		// outward rooms have not been deleted yet
+	}
+
+	return 0;	// successful in deallocating the entire complex
+}
+
+// function for seeking which rooms to further delete
+void seek(room * room_ptr, char coord_opp)
+{
+	char coordinate[NUM_DOORS] = {'N', 'S', 'E', 'W'};
+
+	for(int i = 0; i < NUM_DOORS; i++)
+	{
+		if(coordinate[i] != coord_opp)		// this is to avoid backtracking (which would cause infinite recursion)
+		{
+			switch (coordinate[i])
+			{
+				case 'N':
+					if(room_ptr->north != NULL)
+						roomDelete(room_ptr->north, 'N');
+					break;
+				case 'S':
+					if(room_ptr->south != NULL)
+						roomDelete(room_ptr->south, 'S');
+					break;
+				case 'E':
+					if(room_ptr->east != NULL)
+						roomDelete(room_ptr->east, 'E');
+					break;
+				case 'W':
+					if(room_ptr->west != NULL)
+						roomDelete(room_ptr->west, 'W');
+					break;
+				default:
+					cout << "There was an error deallocating for the room at location: " << room_ptr << endl;
+			}
+		}
+	}
+}
+
+// function that severs the connections between the current room and the previous room
+void sever_connections(room * room_ptr, char coord)
+{
+	switch(coord)
+	{
+		case 'N':
+			room_ptr->south->north = NULL;
+			room_ptr->south = NULL;
+			break;
+		case 'S':
+			room_ptr->north->south = NULL;
+			room_ptr->north = NULL;
+			break;
+		case 'E':
+			room_ptr->west->east = NULL;
+			room_ptr->west = NULL;
+			break;
+		case 'W':
+			room_ptr->east->west = NULL;
+			room_ptr->east = NULL;
+			break;
+		default:
+			cout << "There was a problem with severing the connection for the room at location: " << room_ptr << endl;
 	}
 }
